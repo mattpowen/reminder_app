a8780777f300bf89b79ff4eb6d5996d214216686
Matt Owen <matt.p.owen@gmail.com>
2020-12-11 20:55:20 +0000

    removed migrations and updated admin

--- appointment_app/appointment/admin.py
+++ appointment_app/appointment/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Appointment
+from .models import Appointment, Slot
 
 admin.site.register(Appointment)
+admin.site.register(Slot)
--- appointment_app/appointment/models.py
+++ appointment_app/appointment/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Slots(models.Model):
+class Slot(models.Model):
     day = models.DateField()
     start = models.TimeField()
     end = models.TimeField()
@@ -14,16 +14,16 @@ class Slots(models.Model):
         if self.start < self.end:
             print("Start time shouldn't be after end time")
         else:
-            super(Slots, self).save(*args, **kwargs)
+            super(Slot, self).save(*args, **kwargs)
 
 
 class Appointment(models.Model):
     client = models.ForeignKey("client.Client", on_delete=models.CASCADE)
-    appointment_slot = models.ForeignKey(Slots, on_delete=models.RESTRICT)
+    appointment_slot = models.ForeignKey(Slot, on_delete=models.RESTRICT)
     notes = models.TextField(null=True, blank=True)
 
     def __str__(self):
-        return "{} - {}".format(client, appointment_slot)
+        return "{} - {}".format(self.client, self.appointment_slot)
 
     def save(self):
         if Appointment.objects.filter(appointment_slot=self.appointment_slot).exists():
@@ -34,5 +34,8 @@ class Appointment(models.Model):
 
 class AppointmentReminders(models.Model):
     appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
-    reminder = models.DateField()
+    reminder_date = models.DateField()
     reminder_sent = models.DateTimeField()
+
+    def __str__(self):
+        return "{} {}".format(self.appointment, self.reminder_date)
