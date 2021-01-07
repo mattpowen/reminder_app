476ac2c84b0da4e60be56d743f5f523b69806b79
Matt Owen <matt.p.owen@gmail.com>
2021-01-07 20:39:53 +0000

    changed the max length of the telephone field

--- appointment_app/client/models.py
+++ appointment_app/client/models.py
@@ -13,8 +13,11 @@ class Client(models.Model):
     city = models.CharField(max_length=255, null=True, blank=True)
     postcode = models.CharField(max_length=10, null=True, blank=True)
     telephone_number = models.CharField(max_length=20, null=True, blank=True)
-    mobile_number = models.CharField(max_length=10, null=True, blank=True)
+    mobile_number = models.CharField(max_length=11, null=True, blank=True)
     email_address = models.EmailField(null=True, blank=True)
 
     def __str__(self):
         return "{} {}".format(self.firstname, self.surname)
+
+    def get_mobile_number(self):
+        return self.mobile_number[1:]
